12ac08f546c4e17e8542ecf17601f184b67ec7bd
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-13 21:10:51 +0200

    Modify project naming scheme.

---
+++ learning_tailwind/README.md
@@ -0,0 +1,1 @@
+Going through the material from: [Tailwind CSS From Scratch](https://www.udemy.com/course/tailwind-from-scratch/).
---
+++ learning_tailwind/webpack-tailwind-starter/src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+
+document.body.className =
+  "bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center";
