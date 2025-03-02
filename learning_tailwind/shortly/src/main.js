b8b3cf560552e4d639ab18ad34a4652100d45ef9
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-02 19:19:19 +0200

    Add hamburger menu.

--- learning_tailwind/shortly/src/main.js
+++ learning_tailwind/shortly/src/main.js
@@ -29,3 +29,13 @@ function validateURL(url) {
 }
 
 linkSubmitBtn.addEventListener("click", formSubmit);
+
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+menuBtn.addEventListener("click", navToggle);
+
+function navToggle() {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
