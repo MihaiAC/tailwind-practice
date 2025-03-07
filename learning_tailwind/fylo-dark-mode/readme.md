adf422603f8c6cee34c40432c0481e023bf781c1
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-07 21:31:55 +0200

    Add fonts, images, and some configs.

---
+++ learning_tailwind/fylo-dark-mode/readme.md
@@ -0,0 +1,3 @@
+# Fylo Webpage
+
+Source: [Frontend Mentor Challenge](https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw)
--- learning_tailwind/fylo-dark-mode/tailwind.config.js
+++ learning_tailwind/fylo-dark-mode/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+/* Omitted variants - it seems it's no longer needed? */
 module.exports = {
   content: ["./**/*.html", "./src/**/*.js"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
+  fontFamily: {
+    sans: ["Raleway", "sans-serif"],
+    opensans: ["Open Sans", "sans-serif"],
+  },
+  backgroundImage: (theme) => ({
+    "logo-dark-mode": "url('../images/logo-dark-mode.svg')",
+    "logo-light-mode": "url('../images/logo-light-mode.svg')",
+    "curvy-dark-mode": "url('../images/bg-curvy-dark-mode.svg')",
+    "curvy-light-mode": "url('../images/bg-curvy-light-mode.svg')",
+  }),
   plugins: [],
 };
