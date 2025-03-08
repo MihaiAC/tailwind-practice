b8c0c4a3fff66739f712194d5e8756d5adc26f57
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-08 22:49:29 +0200

    Add theme toggle.

--- learning_tailwind/fylo-dark-mode/src/main.js
+++ learning_tailwind/fylo-dark-mode/src/main.js
@@ -1,1 +1,33 @@
-import "./style.css"
+import "./style.css";
+
+function setTheme(theme) {
+  document.documentElement.classList.remove("light", "dark");
+  document.documentElement.classList.add(theme);
+}
+
+// Get stored or system theme.
+// Default: dark theme + save it.
+let initialTheme = localStorage.getItem("theme");
+if (!initialTheme) {
+  initialTheme = window.matchMedia("(prefers-color-scheme: light)").matches
+    ? "light"
+    : "dark";
+  localStorage.setItem("theme", initialTheme);
+}
+
+// Set initial theme.
+setTheme(initialTheme);
+
+// Add event listener to theme toggling button.
+const toggleButton = document.getElementById("light-switch");
+toggleButton.addEventListener("click", () => {
+  // Calculate new theme.
+  const currentTheme = localStorage.getItem("theme") || "dark";
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  // Remove old theme, apply the new one.
+  setTheme(newTheme);
+
+  // Set the new theme in localStorage.
+  localStorage.setItem("theme", newTheme);
+});
