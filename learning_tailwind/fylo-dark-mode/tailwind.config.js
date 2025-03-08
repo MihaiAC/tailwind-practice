c76a62df299cddf033ff467b5d096f7cdd9733a2
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-08 23:35:40 +0200

    Dark mode styling first pass.

--- learning_tailwind/fylo-dark-mode/tailwind.config.js
+++ learning_tailwind/fylo-dark-mode/tailwind.config.js
@@ -4,17 +4,19 @@ module.exports = {
   content: ["./**/*.html", "./src/**/*.js"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "logo-dark-mode": "url('../images/logo-dark-mode.svg')",
+        "logo-light-mode": "url('../images/logo-light-mode.svg')",
+        "curvy-dark-mode": "url('../images/bg-curvy-dark-mode.svg')",
+        "curvy-light-mode": "url('../images/bg-curvy-light-mode.svg')",
+      }),
+    },
   },
   fontFamily: {
     sans: ["Raleway", "sans-serif"],
     opensans: ["Open Sans", "sans-serif"],
   },
-  backgroundImage: (theme) => ({
-    "logo-dark-mode": "url('../images/logo-dark-mode.svg')",
-    "logo-light-mode": "url('../images/logo-light-mode.svg')",
-    "curvy-dark-mode": "url('../images/bg-curvy-dark-mode.svg')",
-    "curvy-light-mode": "url('../images/bg-curvy-light-mode.svg')",
-  }),
+
   plugins: [],
 };
