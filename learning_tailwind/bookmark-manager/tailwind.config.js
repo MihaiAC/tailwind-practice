5fa850ca6fa0606ed9140cbdcc0026f0e7b0cc99
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-09 22:20:45 +0200

    Add colors and font to config.

--- learning_tailwind/bookmark-manager/tailwind.config.js
+++ learning_tailwind/bookmark-manager/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 69%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayBlue: "hsl(229, 8%, 60%)",
+        darkBlue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
